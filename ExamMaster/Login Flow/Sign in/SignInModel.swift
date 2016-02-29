1f1257af7a3fae3440ecc57df7b6485fa80d1005
Aleksey Chernish <achernish85@gmail.com>
2016-02-29 18:36:27 +0700

    removed a comment

--- ExamMaster/Login Flow/Sign in/SignInModel.swift
+++ ExamMaster/Login Flow/Sign in/SignInModel.swift
@@ -6,11 +6,6 @@
 //  Copyright © 2016 aleksey chernish. All rights reserved.
 //
 
-
-//Обратный вызов реализован при помощи Bubble Notification
-//LoginFlowModel объявляет свой домен для этих уведомлений и перехватывает их на всплытии.
-//Удобно для доставки сообщения вверх через несколько посредников
-
 import Foundation
 import ModelsTreeKit
 
